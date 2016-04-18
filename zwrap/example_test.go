573baa1c3c310946313089d1ed2c1fd85a28f48f
Akshay Shah <shah@uber.com>
2016-04-18 08:46:57 -0700

    Add a stdlib-compatible wrapper

---
+++ zwrap/example_test.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package zwrap_test
+
+import (
+	"github.com/uber-common/zap"
+	"github.com/uber-common/zap/zwrap"
+)
+
+func Example() {
+	zapLogger := zap.NewJSON()
+	// Stub the current time in tests.
+	zapLogger.StubTime()
+
+	// Wrap our structured logger to mimic the standard library's log.Logger.
+	// We also specify that we want all calls to the standard logger's Print
+	// family of methods to log at zap's Warn level.
+	stdLogger, err := zwrap.Standardize(zapLogger, zap.Warn)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// The wrapped logger has the usual Print, Panic, and Fatal families of
+	// methods.
+	stdLogger.Printf("Encountered %d errors.", 0)
+
+	// Output:
+	// {"msg":"Encountered 0 errors.","level":"warn","ts":0,"fields":{}}
+}
