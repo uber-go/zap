a5a3dacd4a2eb85f34bdb0b2f4aab348546893e0
Akshay Shah <shah@uber.com>
2016-11-21 09:36:28 -0800

    WIP, addressing comments on @akabos's PR

--- sugar.go
+++ sugar.go
@@ -21,180 +21,209 @@
 package zap
 
 import (
-	"errors"
 	"fmt"
 	"time"
 )
 
-// Sugar is a wrapper around core logger with less verbose API
-type Sugar interface {
-	// Level returns the minimum enabled log level
-	Level() Level
-
-	// SetLevel changes the level of this logger, as well as all its ancestors
-	// and descendants. This makes it easy to change the log level at runtime
-	// without restarting your application.
-	SetLevel(Level)
-
-	// With creates a child logger, and optionally add some context to that
-	// logger
-	With(...interface{}) Sugar
-
-	// Log a message at the given level. Messages include any context that's
-	// accumulated on the logger, as well as any fields added at the log site.
-	Log(Level, string, ...interface{})
-	Debug(string, ...interface{})
-	Info(string, ...interface{})
-	Warn(string, ...interface{})
-	Error(string, ...interface{})
-	Panic(string, ...interface{})
-	Fatal(string, ...interface{})
-
-	// DFatal logs at the Fatal level if the logger is in development mode (via
-	// the Development option), otherwise it logs at the Error level.
-	DFatal(string, ...interface{})
-}
-
-type sugar struct {
+// A SugaredLogger wraps the core Logger functionality in a slower, but less
+// verbose, API.
+type SugaredLogger struct {
 	core Logger
 }
 
-func (s *sugar) internalError(msg string) {
-	s.core.(*logger).internalError(msg)
+// Sugar converts a Logger to a SugaredLogger.
+func Sugar(core Logger) *SugaredLogger {
+	return &SugaredLogger{core}
 }
 
-// NewSugar is a constructor for Sugar
-func NewSugar(core Logger) Sugar {
-	return &sugar{core}
+// Desugar unwraps a SugaredLogger, exposing the original Logger.
+func Desugar(s *SugaredLogger) Logger {
+	return s.core
 }
 
-func (s *sugar) Level() Level {
-	return s.core.Level()
+// With adds a variadic number of key-value pairs to the logging context.
+// Even-indexed arguments are treated as keys, and are converted to strings
+// (with fmt.Sprint) if necessary. The keys are then zipped with the
+// odd-indexed values into typed fields, falling back to a reflection-based
+// encoder if necessary.
+//
+// For example,
+//   sugaredLogger.With(
+//     "hello", "world",
+//     "failure", errors.New("oh no"),
+//     "count", 42,
+//     "user", User{name: "alice"},
+//  )
+// is the equivalent of
+//   coreLogger.With(
+//     String("hello", "world"),
+//     String("failure", "oh no"),
+//     Int("count", 42),
+//     Object("user", User{name: "alice"}),
+//   )
+func (s *SugaredLogger) With(args ...interface{}) *SugaredLogger {
+	return &SugaredLogger{core: s.core.With(sweetenFields(args, s.core)...)}
 }
 
-func (s *sugar) SetLevel(lvl Level) {
-	s.core.SetLevel(lvl)
+// WithStack adds a complete stack trace to the logger's context, using the key
+// "stacktrace".
+func (s *SugaredLogger) WithStack() *SugaredLogger {
+	return &SugaredLogger{core: s.core.With(Stack())}
 }
 
-func (s *sugar) With(args ...interface{}) Sugar {
-	fields, err := getSugarFields(args...)
-	if err != nil {
-		s.internalError(err.Error())
-	}
-	return NewSugar(s.core.With(fields...))
+// Debug logs a message and some key-value pairs at DebugLevel. Keys and values
+// are treated as they are in the With method.
+func (s *SugaredLogger) Debug(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Debug(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
 }
 
-func getSugarFields(args ...interface{}) ([]Field, error) {
-	var (
-		noErrArgs []interface{}
+// Debugf uses fmt.Sprintf to construct a dynamic message and logs it at
+// DebugLevel. It doesn't add to the message's structured context.
+func (s *SugaredLogger) Debugf(template string, args ...interface{}) {
+	s.Debug(fmt.Sprintf(template, args...))
+}
 
-		ii    int
-		key   string
-		value interface{}
-	)
-	fields := make([]Field, 0, len(args)/2)
+// Info logs a message and some key-value pairs at InfoLevel. Keys and values
+// are treated as they are in the With method.
+func (s *SugaredLogger) Info(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Info(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
 
+// Infof uses fmt.Sprintf to construct a dynamic message and logs it at
+// InfoLevel. It doesn't add to the message's structured context.
+func (s *SugaredLogger) Infof(template string, args ...interface{}) {
+	s.Info(fmt.Sprintf(template, args...))
+}
+
+// Warn logs a message and some key-value pairs at WarnLevel. Keys and values
+// are treated as they are in the With method.
+func (s *SugaredLogger) Warn(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Warn(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
+
+// Warnf uses fmt.Sprintf to construct a dynamic message and logs it at
+// WarnLevel. It doesn't add to the message's structured context.
+func (s *SugaredLogger) Warnf(template string, args ...interface{}) {
+	s.Warn(fmt.Sprintf(template, args...))
+}
+
+// Error logs a message and some key-value pairs at ErrorLevel. Keys and values
+// are treated as they are in the With method.
+func (s *SugaredLogger) Error(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Error(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
+
+// Errorf uses fmt.Sprintf to construct a dynamic message and logs it at
+// ErrorLevel. It doesn't add to the message's structured context.
+func (s *SugaredLogger) Errorf(template string, args ...interface{}) {
+	s.Error(fmt.Sprintf(template, args...))
+}
+
+// Panic logs a message and some key-value pairs at PanicLevel, then panics.
+// Keys and values are treated as they are in the With method.
+func (s *SugaredLogger) Panic(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Panic(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
+
+// Panicf uses fmt.Sprintf to construct a dynamic message and logs it at
+// PanicLevel, then panics. It doesn't add to the message's structured context.
+func (s *SugaredLogger) Panicf(template string, args ...interface{}) {
+	s.Panic(fmt.Sprintf(template, args...))
+}
+
+// Fatal logs a message and some key-value pairs at FatalLevel, then calls
+// os.Exit(1). Keys and values are treated as they are in the With method.
+func (s *SugaredLogger) Fatal(msg interface{}, keysAndValues ...interface{}) {
+	s.core.Fatal(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
+
+// Fatalf uses fmt.Sprintf to construct a dynamic message and logs it at
+// FatalLevel, then calls os.Exit(1). It doesn't add to the message's
+// structured context.
+func (s *SugaredLogger) Fatalf(template string, args ...interface{}) {
+	s.Fatal(fmt.Sprintf(template, args...))
+}
+
+// DFatal logs a message and some key-value pairs using the underlying logger's
+// DFatal method. Keys and values are treated as they are in the With
+// method. (See Logger.DFatal for details.)
+func (s *SugaredLogger) DFatal(msg interface{}, keysAndValues ...interface{}) {
+	s.core.DFatal(sweetenMsg(msg), sweetenFields(keysAndValues, s.core)...)
+}
+
+// DFatalf uses fmt.Sprintf to construct a dynamic message, which is passed to
+// the underlying Logger's DFatal method. (See Logger.DFatal for details.) It
+// doesn't add to the message's structured context.
+func (s *SugaredLogger) DFatalf(template string, args ...interface{}) {
+	s.DFatal(fmt.Sprintf(template, args...))
+}
+
+func sweetenFields(args []interface{}, errLogger Logger) []Field {
 	if len(args) == 0 {
-		return fields, nil
+		return nil
+	}
+	if len(args)%2 == 1 {
+		errLogger.DFatal(
+			"Passed an odd number of keys and values to SugaredLogger, ignoring last.",
+			Object("ignored", args[len(args)-1]),
+		)
 	}
 
-	switch err := args[0].(type) {
-	case error:
-		fields = append(fields, Error(err))
-		noErrArgs = args[1:]
-	default:
-		noErrArgs = args
-	}
+	fields := make([]Field, len(args)/2)
+	for i := range fields {
+		key := sweetenMsg(args[2*i])
 
-	if (len(noErrArgs) % 2) != 0 {
-		return nil, errors.New("invalid number of arguments")
-	}
-
-	for ii, value = range noErrArgs {
-		if (ii % 2) == 0 {
-			switch value.(type) {
-			case string:
-				key = value.(string)
-			default:
-				return nil, errors.New("field name must be string")
-			}
-		} else {
-			// TODO: Add LogMarshaler support, it once been here, but
-			//       had to be removed because type switch won't catch
-			//       this intarface properly for some mystical reason.
-			switch v := value.(type) {
-			case error:
-				return nil, errors.New("error can only be the first argument")
-			case bool:
-				fields = append(fields, Bool(key, v))
-			case float64:
-				fields = append(fields, Float64(key, v))
-			case int:
-				fields = append(fields, Int(key, v))
-			case int64:
-				fields = append(fields, Int64(key, v))
-			case uint:
-				fields = append(fields, Uint(key, v))
-			case uint64:
-				fields = append(fields, Uint64(key, v))
-			case string:
-				fields = append(fields, String(key, v))
-			case time.Time:
-				fields = append(fields, Time(key, v))
-			case time.Duration:
-				fields = append(fields, Duration(key, v))
-			case fmt.Stringer:
-				fields = append(fields, Stringer(key, v))
-			default:
-				fields = append(fields, Object(key, value))
-			}
+		switch val := args[2*i+1].(type) {
+		case LogMarshaler:
+			fields[i] = Marshaler(key, val)
+		case bool:
+			fields[i] = Bool(key, val)
+		case float64:
+			fields[i] = Float64(key, val)
+		case float32:
+			fields[i] = Float64(key, float64(val))
+		case int:
+			fields[i] = Int(key, val)
+		case int64:
+			fields[i] = Int64(key, val)
+		case int32:
+			fields[i] = Int64(key, int64(val))
+		case int16:
+			fields[i] = Int64(key, int64(val))
+		case int8:
+			fields[i] = Int64(key, int64(val))
+		case uint:
+			fields[i] = Uint(key, val)
+		case uint64:
+			fields[i] = Uint64(key, val)
+		case uint32:
+			fields[i] = Uint64(key, uint64(val))
+		case uint16:
+			fields[i] = Uint64(key, uint64(val))
+		case uint8:
+			fields[i] = Uint64(key, uint64(val))
+		case uintptr:
+			fields[i] = Uintptr(key, val)
+		case string:
+			fields[i] = String(key, val)
+		case time.Time:
+			fields[i] = Time(key, val)
+		case time.Duration:
+			fields[i] = Duration(key, val)
+		case error:
+			fields[i] = String(key, val.Error())
+		case fmt.Stringer:
+			fields[i] = String(key, val.String())
+		default:
+			fields[i] = Object(key, val)
 		}
 	}
-	return fields, nil
+	return fields
 }
 
-func (s *sugar) Log(lvl Level, msg string, args ...interface{}) {
-	var (
-		fields []Field
-		err    error
-	)
-	if cm := s.core.Check(lvl, msg); cm.OK() {
-		fields, err = getSugarFields(args...)
-		if err != nil {
-			s.internalError(err.Error())
-		}
-		cm.Write(fields...)
+func sweetenMsg(msg interface{}) string {
+	if m, ok := msg.(string); ok {
+		return m
 	}
-}
-
-func (s *sugar) Debug(msg string, args ...interface{}) {
-	s.Log(DebugLevel, msg, args...)
-}
-
-func (s *sugar) Info(msg string, args ...interface{}) {
-	s.Log(InfoLevel, msg, args...)
-}
-
-func (s *sugar) Warn(msg string, args ...interface{}) {
-	s.Log(WarnLevel, msg, args...)
-}
-
-func (s *sugar) Error(msg string, args ...interface{}) {
-	s.Log(ErrorLevel, msg, args...)
-}
-
-func (s *sugar) Panic(msg string, args ...interface{}) {
-	s.Log(PanicLevel, msg, args...)
-}
-
-func (s *sugar) Fatal(msg string, args ...interface{}) {
-	s.Log(FatalLevel, msg, args...)
-}
-
-func (s *sugar) DFatal(msg string, args ...interface{}) {
-	fields, err := getSugarFields(args...)
-	if err != nil {
-		s.internalError(err.Error())
-	}
-	s.core.DFatal(msg, fields...)
+	return fmt.Sprint(msg)
 }
