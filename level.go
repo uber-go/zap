835580952087097d9328254dbe0aee4714ce540d
Joshua T Corbin <joshua@uber.com>
2016-11-30 11:37:31 -0800

    Add DPanicLevel

--- level.go
+++ level.go
@@ -63,6 +63,8 @@ const (
 	// ErrorLevel logs are high-priority. If an application is running smoothly,
 	// it shouldn't generate any error-level logs.
 	ErrorLevel
+	// DPanicLevel logs an error message, but panics only in development.
+	DPanicLevel
 	// PanicLevel logs a message, then panics.
 	PanicLevel
 	// FatalLevel logs a message, then calls os.Exit(1).
@@ -80,6 +82,8 @@ func (l Level) String() string {
 		return "warn"
 	case ErrorLevel:
 		return "error"
+	case DPanicLevel:
+		return "dpanic"
 	case PanicLevel:
 		return "panic"
 	case FatalLevel:
@@ -114,6 +118,8 @@ func (l *Level) UnmarshalText(text []byte) error {
 		*l = WarnLevel
 	case "error":
 		*l = ErrorLevel
+	case "dpanic":
+		*l = DPanicLevel
 	case "panic":
 		*l = PanicLevel
 	case "fatal":
