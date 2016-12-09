bc7f9e693cfdd4741fd81f45370084e320195a73
Joshua T Corbin <joshua@uber.com>
2016-12-09 15:30:27 -0800

    Add MultiWriteSyncer benchmark

---
+++ writer_bench_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package zap_test
+
+import (
+	"testing"
+
+	"go.uber.org/zap"
+)
+
+func BenchmarkMultiWriteSyncer2(b *testing.B) {
+	log := zap.New(
+		zap.NewJSONEncoder(),
+		zap.DebugLevel,
+		zap.Output(zap.MultiWriteSyncer(zap.Discard, zap.Discard)),
+	)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("yup")
+		}
+	})
+}
+
+func BenchmarkMultiWriteSyncer4(b *testing.B) {
+	log := zap.New(
+		zap.NewJSONEncoder(),
+		zap.DebugLevel,
+		zap.Output(zap.MultiWriteSyncer(zap.Discard, zap.Discard, zap.Discard, zap.Discard)),
+	)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			log.Info("yup")
+		}
+	})
+}
