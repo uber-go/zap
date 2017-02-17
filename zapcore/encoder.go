4be2d84df3eaa9a900090bce881bd78ccd065968
Peter Edge <peter.edge@gmail.com>
2017-02-17 17:36:25 +0100

    Fix json and yaml tags (#309)
    Tags for multiple encodings must be space-separated, not comma-separated.

--- zapcore/encoder.go
+++ zapcore/encoder.go
@@ -141,18 +141,18 @@ func (e *DurationEncoder) UnmarshalText(text []byte) error {
 // zapcore.
 type EncoderConfig struct {
 	// Set the keys used for each log entry.
-	MessageKey    string `json:"messageKey",yaml:"messageKey"`
-	LevelKey      string `json:"levelKey",yaml:"levelKey"`
-	TimeKey       string `json:"timeKey",yaml:"timeKey"`
-	NameKey       string `json:"nameKey",yaml:"nameKey"`
-	CallerKey     string `json:"callerKey",yaml:"callerKey"`
-	StacktraceKey string `json:"stacktraceKey",yaml:"stacktraceKey"`
+	MessageKey    string `json:"messageKey" yaml:"messageKey"`
+	LevelKey      string `json:"levelKey" yaml:"levelKey"`
+	TimeKey       string `json:"timeKey" yaml:"timeKey"`
+	NameKey       string `json:"nameKey" yaml:"nameKey"`
+	CallerKey     string `json:"callerKey" yaml:"callerKey"`
+	StacktraceKey string `json:"stacktraceKey" yaml:"stacktraceKey"`
 	// Configure the primitive representations of common complex types. For
 	// example, some users may want all time.Times serialized as floating-point
 	// seconds since epoch, while others may prefer ISO8601 strings.
-	EncodeLevel    LevelEncoder    `json:"levelEncoder",yaml:"levelEncoder"`
-	EncodeTime     TimeEncoder     `json:"timeEncoder",yaml:"timeEncoder"`
-	EncodeDuration DurationEncoder `json:"durationEncoder",yaml:"durationEncoder"`
+	EncodeLevel    LevelEncoder    `json:"levelEncoder" yaml:"levelEncoder"`
+	EncodeTime     TimeEncoder     `json:"timeEncoder" yaml:"timeEncoder"`
+	EncodeDuration DurationEncoder `json:"durationEncoder" yaml:"durationEncoder"`
 }
 
 // ObjectEncoder is a strongly-typed, encoding-agnostic interface for adding a
