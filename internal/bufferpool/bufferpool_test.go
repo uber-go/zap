1b7cf6e2a2879c52e16d16501946fb3d14d498ae
Will Hughes <willhughes01@gmail.com>
2017-03-06 09:27:42 -0800

    Make bufferpool test more complete (#340)
    To test the length and capacity of the pooled buffers properly, write some dummy data into them on each iteration.

--- internal/bufferpool/bufferpool_test.go
+++ internal/bufferpool/bufferpool_test.go
@@ -28,6 +28,8 @@ import (
 )
 
 func TestBuffers(t *testing.T) {
+	const dummyData = "dummy data"
+
 	var wg sync.WaitGroup
 	for g := 0; g < 10; g++ {
 		wg.Add(1)
@@ -36,6 +38,10 @@ func TestBuffers(t *testing.T) {
 				buf := Get()
 				assert.Zero(t, buf.Len(), "Expected truncated buffer")
 				assert.NotZero(t, buf.Cap(), "Expected non-zero capacity")
+
+				buf.AppendString(dummyData)
+				assert.Equal(t, buf.Len(), len(dummyData), "Expected buffer to contain dummy data")
+
 				Put(buf)
 			}
 			wg.Done()
