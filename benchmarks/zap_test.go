30cabebbe45c41fc2f46ad0af73c3e099da08a5c
Akshay Shah <shah@uber.com>
2017-10-30 13:56:15 -0700

    Migrate benchmarks package to use ztest
    Migrate the benchmarks package to use the internal `ztest` helpers
    instead of the exported `zaptest` package.

--- benchmarks/zap_test.go
+++ benchmarks/zap_test.go
@@ -27,8 +27,8 @@ import (
 
 	"go.uber.org/multierr"
 	"go.uber.org/zap"
+	"go.uber.org/zap/internal/ztest"
 	"go.uber.org/zap/zapcore"
-	"go.uber.org/zap/zaptest"
 )
 
 var (
@@ -110,7 +110,7 @@ func newZapLogger(lvl zapcore.Level) *zap.Logger {
 	enc := zapcore.NewJSONEncoder(ec)
 	return zap.New(zapcore.NewCore(
 		enc,
-		&zaptest.Discarder{},
+		&ztest.Discarder{},
 		lvl,
 	))
 }
