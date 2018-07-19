d7fbc49a7f3f18d9bddc5afbe0486aed1d7c8689
Akshay Shah <akshayjshah@users.noreply.github.com>
2018-07-19 14:12:42 -0700

    Make user-supplied sinks operate on URIs (#606)
    For future extensibility, make user-supplied factories for log sinks
    operate on URIs. Each user-supplied factory owns a scheme, and
    double-registering constructors for a scheme is an error. For
    back-compat, zap automatically registers a factory for the `file` scheme
    and treats URIs without a scheme as though they were for files.

--- sink_test.go
+++ sink_test.go
@@ -21,56 +21,80 @@
 package zap
 
 import (
-	"errors"
-	"os"
+	"bytes"
+	"io/ioutil"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.uber.org/zap/zapcore"
 )
 
 func TestRegisterSink(t *testing.T) {
-	tests := []struct {
-		name      string
-		key       string
-		factory   func() (Sink, error)
-		wantError bool
-	}{
-		{"valid", "valid", func() (Sink, error) { return nopCloserSink{os.Stdout}, nil }, false},
-		{"empty", "", func() (Sink, error) { return nopCloserSink{os.Stdout}, nil }, true},
-		{"stdout", "stdout", func() (Sink, error) { return nopCloserSink{os.Stdout}, nil }, true},
+	const (
+		memScheme = "m"
+		nopScheme = "no-op.1234"
+	)
+	var memCalls, nopCalls int
+
+	buf := bytes.NewBuffer(nil)
+	memFactory := func(u *url.URL) (Sink, error) {
+		assert.Equal(t, u.Scheme, memScheme, "Scheme didn't match registration.")
+		memCalls++
+		return nopCloserSink{zapcore.AddSync(buf)}, nil
+	}
+	nopFactory := func(u *url.URL) (Sink, error) {
+		assert.Equal(t, u.Scheme, nopScheme, "Scheme didn't match registration.")
+		nopCalls++
+		return nopCloserSink{zapcore.AddSync(ioutil.Discard)}, nil
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := RegisterSink(tt.key, tt.factory)
-			if tt.wantError {
-				assert.NotNil(t, err)
-			} else {
-				assert.Nil(t, err)
-				assert.NotNil(t, _sinkFactories[tt.key], "expected the factory to be present")
-			}
-		})
-	}
+	defer resetSinkRegistry()
+
+	require.NoError(t, RegisterSink(strings.ToUpper(memScheme), memFactory), "Failed to register scheme %q.", memScheme)
+	require.NoError(t, RegisterSink(nopScheme, nopFactory), "Failed to register scheme %q.", memScheme)
+
+	sink, close, err := Open(
+		memScheme+"://somewhere",
+		nopScheme+"://somewhere-else",
+	)
+	assert.NoError(t, err, "Unexpected error opening URLs with registered schemes.")
+
+	defer close()
+
+	assert.Equal(t, 1, memCalls, "Unexpected number of calls to memory factory.")
+	assert.Equal(t, 1, nopCalls, "Unexpected number of calls to no-op factory.")
+
+	_, err = sink.Write([]byte("foo"))
+	assert.NoError(t, err, "Failed to write to combined WriteSyncer.")
+	assert.Equal(t, "foo", buf.String(), "Unexpected buffer contents.")
 }
 
-func TestNewSink(t *testing.T) {
-	defer resetSinkRegistry()
-	errTestSink := errors.New("test erroring")
-	err := RegisterSink("errors", func() (Sink, error) { return nil, errTestSink })
-	assert.Nil(t, err)
+func TestRegisterSinkErrors(t *testing.T) {
+	nopFactory := func(_ *url.URL) (Sink, error) {
+		return nopCloserSink{zapcore.AddSync(ioutil.Discard)}, nil
+	}
 	tests := []struct {
-		key string
-		err error
+		scheme string
+		err    string
 	}{
-		{"stdout", nil},
-		{"errors", errTestSink},
-		{"nonexistent", &errSinkNotFound{"nonexistent"}},
+		{"", "empty string"},
+		{"FILE", "already registered"},
+		{"42", "not a valid scheme"},
+		{"http*", "not a valid scheme"},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.key, func(t *testing.T) {
-			_, err := newSink(tt.key)
-			assert.Equal(t, tt.err, err)
+		t.Run("scheme-"+tt.scheme, func(t *testing.T) {
+			defer resetSinkRegistry()
+
+			err := RegisterSink(tt.scheme, nopFactory)
+			if assert.Error(t, err, "expected error") {
+				assert.Contains(t, err.Error(), tt.err, "unexpected error")
+			}
 		})
 	}
 }
