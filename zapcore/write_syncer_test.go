b1a95ffa4318d1b9e860a266f64274dbda0e78f3
liqi <hnlq.sysu@gmail.com>
2021-01-06 15:52:19 +0800

    remove errorWriter

--- zapcore/write_syncer_test.go
+++ zapcore/write_syncer_test.go
@@ -39,10 +39,6 @@ type writeSyncSpy struct {
 	ztest.Syncer
 }
 
-type errorWriter struct{}
-
-func (*errorWriter) Write([]byte) (int, error) { return 0, errors.New("unimplemented") }
-
 func requireWriteWorks(t testing.TB, ws WriteSyncer) {
 	n, err := ws.Write([]byte("foo"))
 	require.NoError(t, err, "Unexpected error writing to WriteSyncer.")
@@ -117,7 +113,7 @@ func TestBufferWriter(t *testing.T) {
 	})
 
 	t.Run("flush error", func(t *testing.T) {
-		ws, close := Buffer(AddSync(&errorWriter{}), 4, 0)
+		ws, close := Buffer(AddSync(&ztest.FailWriter{}), 4, 0)
 		n, err := ws.Write([]byte("foo"))
 		require.NoError(t, err, "Unexpected error writing to WriteSyncer.")
 		require.Equal(t, 3, n, "Wrote an unexpected number of bytes.")
