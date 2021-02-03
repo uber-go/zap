9f3c0ee8caf4e5ba2370654c251b27a4a3dc71c5
Shubhendra Singh Chauhan <withshubh@gmail.com>
2021-02-04 02:01:18 +0530

    Fix: code quality issues using DeepSource (#907)

--- logger.go
+++ logger.go
@@ -335,7 +335,7 @@ func getCallerFrame(skip int) (frame runtime.Frame, ok bool) {
 	const skipOffset = 2 // skip getCallerFrame and Callers
 
 	pc := make([]uintptr, 1)
-	numFrames := runtime.Callers(skip+skipOffset, pc[:])
+	numFrames := runtime.Callers(skip+skipOffset, pc)
 	if numFrames < 1 {
 		return
 	}
