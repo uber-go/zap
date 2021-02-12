89e382035d3a984a01e6c9c8be5462f11efac844
Mikhail Mazurskiy <126021+ash2k@users.noreply.github.com>
2021-02-13 08:10:49 +1100

    Add support for grpclog.LoggerV2 (#881)
    gRPC has an updated [logger interface][1] that the v1 API has been
    deprecated in favor of.
    
    [1]: https://pkg.go.dev/google.golang.org/grpc/grpclog#LoggerV2
    
    This adds support for it to the existing gRPC adapter in Zap.
    
    Fixes #534 
    Closes #538
    
    Co-authored-by: Prashant Varanasi <github@prashantv.com>
    Co-authored-by: Abhinav Gupta <abg@uber.com>

--- zapgrpc/zapgrpc_test.go
+++ zapgrpc/zapgrpc_test.go
@@ -21,6 +21,7 @@
 package zapgrpc
 
 import (
+	"fmt"
 	"testing"
 
 	"go.uber.org/zap"
@@ -33,44 +34,200 @@ import (
 func TestLoggerInfoExpected(t *testing.T) {
 	checkMessages(t, zapcore.DebugLevel, nil, zapcore.InfoLevel, []string{
 		"hello",
-		"world",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
 		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
+		"hello",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
+		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
 	}, func(logger *Logger) {
+		logger.Info("hello")
+		logger.Info("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Infof("%s world", "hello")
+		logger.Infoln()
+		logger.Infoln("foo")
+		logger.Infoln("foo", "bar")
+		logger.Infoln("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
 		logger.Print("hello")
-		logger.Printf("world")
+		logger.Print("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Printf("%s world", "hello")
+		logger.Println()
 		logger.Println("foo")
+		logger.Println("foo", "bar")
+		logger.Println("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
 	})
 }
 
 func TestLoggerDebugExpected(t *testing.T) {
 	checkMessages(t, zapcore.DebugLevel, []Option{WithDebug()}, zapcore.DebugLevel, []string{
 		"hello",
-		"world",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
 		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
 	}, func(logger *Logger) {
 		logger.Print("hello")
-		logger.Printf("world")
+		logger.Print("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Printf("%s world", "hello")
+		logger.Println()
 		logger.Println("foo")
+		logger.Println("foo", "bar")
+		logger.Println("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
 	})
 }
 
 func TestLoggerDebugSuppressed(t *testing.T) {
 	checkMessages(t, zapcore.InfoLevel, []Option{WithDebug()}, zapcore.DebugLevel, nil, func(logger *Logger) {
 		logger.Print("hello")
-		logger.Printf("world")
+		logger.Printf("%s world", "hello")
+		logger.Println()
 		logger.Println("foo")
+		logger.Println("foo", "bar")
+	})
+}
+
+func TestLoggerWarningExpected(t *testing.T) {
+	checkMessages(t, zapcore.DebugLevel, nil, zapcore.WarnLevel, []string{
+		"hello",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
+		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
+	}, func(logger *Logger) {
+		logger.Warning("hello")
+		logger.Warning("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Warningf("%s world", "hello")
+		logger.Warningln()
+		logger.Warningln("foo")
+		logger.Warningln("foo", "bar")
+		logger.Warningln("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+	})
+}
+
+func TestLoggerErrorExpected(t *testing.T) {
+	checkMessages(t, zapcore.DebugLevel, nil, zapcore.ErrorLevel, []string{
+		"hello",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
+		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
+	}, func(logger *Logger) {
+		logger.Error("hello")
+		logger.Error("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Errorf("%s world", "hello")
+		logger.Errorln()
+		logger.Errorln("foo")
+		logger.Errorln("foo", "bar")
+		logger.Errorln("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
 	})
 }
 
 func TestLoggerFatalExpected(t *testing.T) {
 	checkMessages(t, zapcore.DebugLevel, nil, zapcore.FatalLevel, []string{
 		"hello",
-		"world",
+		"s1s21 2 3s34s56",
+		"hello world",
+		"",
 		"foo",
+		"foo bar",
+		"s1 s2 1 2 3 s3 4 s5 6",
 	}, func(logger *Logger) {
 		logger.Fatal("hello")
-		logger.Fatalf("world")
+		logger.Fatal("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+		logger.Fatalf("%s world", "hello")
+		logger.Fatalln()
 		logger.Fatalln("foo")
+		logger.Fatalln("foo", "bar")
+		logger.Fatalln("s1", "s2", 1, 2, 3, "s3", 4, "s5", 6)
+	})
+}
+
+func TestLoggerV(t *testing.T) {
+	tests := []struct {
+		zapLevel     zapcore.Level
+		grpcEnabled  []int
+		grpcDisabled []int
+	}{
+		{
+			zapLevel:     zapcore.DebugLevel,
+			grpcEnabled:  []int{grpcLvlInfo, grpcLvlWarn, grpcLvlError, grpcLvlFatal},
+			grpcDisabled: []int{}, // everything is enabled, nothing is disabled
+		},
+		{
+			zapLevel:     zapcore.InfoLevel,
+			grpcEnabled:  []int{grpcLvlInfo, grpcLvlWarn, grpcLvlError, grpcLvlFatal},
+			grpcDisabled: []int{}, // everything is enabled, nothing is disabled
+		},
+		{
+			zapLevel:     zapcore.WarnLevel,
+			grpcEnabled:  []int{grpcLvlWarn, grpcLvlError, grpcLvlFatal},
+			grpcDisabled: []int{grpcLvlInfo},
+		},
+		{
+			zapLevel:     zapcore.ErrorLevel,
+			grpcEnabled:  []int{grpcLvlError, grpcLvlFatal},
+			grpcDisabled: []int{grpcLvlInfo, grpcLvlWarn},
+		},
+		{
+			zapLevel:     zapcore.DPanicLevel,
+			grpcEnabled:  []int{grpcLvlFatal},
+			grpcDisabled: []int{grpcLvlInfo, grpcLvlWarn, grpcLvlError},
+		},
+		{
+			zapLevel:     zapcore.PanicLevel,
+			grpcEnabled:  []int{grpcLvlFatal},
+			grpcDisabled: []int{grpcLvlInfo, grpcLvlWarn, grpcLvlError},
+		},
+		{
+			zapLevel:     zapcore.FatalLevel,
+			grpcEnabled:  []int{grpcLvlFatal},
+			grpcDisabled: []int{grpcLvlInfo, grpcLvlWarn, grpcLvlError},
+		},
+	}
+	for _, tst := range tests {
+		for _, grpcLvl := range tst.grpcEnabled {
+			t.Run(fmt.Sprintf("enabled %s %d", tst.zapLevel, grpcLvl), func(t *testing.T) {
+				checkLevel(t, tst.zapLevel, true, func(logger *Logger) bool {
+					return logger.V(grpcLvl)
+				})
+			})
+		}
+		for _, grpcLvl := range tst.grpcDisabled {
+			t.Run(fmt.Sprintf("disabled %s %d", tst.zapLevel, grpcLvl), func(t *testing.T) {
+				checkLevel(t, tst.zapLevel, false, func(logger *Logger) bool {
+					return logger.V(grpcLvl)
+				})
+			})
+		}
+	}
+}
+
+func checkLevel(
+	t testing.TB,
+	enab zapcore.LevelEnabler,
+	expectedBool bool,
+	f func(*Logger) bool,
+) {
+	withLogger(enab, nil, func(logger *Logger, observedLogs *observer.ObservedLogs) {
+		actualBool := f(logger)
+		if expectedBool {
+			require.True(t, actualBool)
+		} else {
+			require.False(t, actualBool)
+		}
 	})
 }
 
@@ -103,13 +260,4 @@ func withLogger(
 ) {
 	core, observedLogs := observer.New(enab)
 	f(NewLogger(zap.New(core), append(opts, withWarn())...), observedLogs)
-}
-
-// withWarn redirects the fatal level to the warn level, which makes testing
-// easier.
-func withWarn() Option {
-	return optionFunc(func(logger *Logger) {
-		logger.fatal = (*zap.SugaredLogger).Warn
-		logger.fatalf = (*zap.SugaredLogger).Warnf
-	})
 }
