c65627f726cdd88688197d4299fb3b4d8170a1e2
Abhinav Gupta <abg@uber.com>
2021-12-14 16:58:09 -0800

    TestJSONCustomReflectedEncoder: parallelize

--- zapcore/json_encoder_test.go
+++ zapcore/json_encoder_test.go
@@ -211,16 +211,19 @@ func TestJSONCustomReflectedEncoder(t *testing.T) {
 		},
 	}
 
-	enc := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
-		NewReflectedEncoder: func(writer io.Writer) zapcore.ReflectedEncoder {
-			return &emptyReflectedEncoder{
-				writer: writer,
-			}
-		},
-	})
-
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			enc := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+				NewReflectedEncoder: func(writer io.Writer) zapcore.ReflectedEncoder {
+					return &emptyReflectedEncoder{
+						writer: writer,
+					}
+				},
+			})
+
 			buf, err := enc.EncodeEntry(zapcore.Entry{
 				Level:      zapcore.DebugLevel,
 				Time:       time.Now(),
