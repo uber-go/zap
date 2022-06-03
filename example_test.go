310a1d88dea82fa160465a5535b2de53c5cd1230
Craig Pastro <craig.pastro@auth0.com>
2022-06-03 09:41:39 -0700

    Use Must in examples

--- example_test.go
+++ example_test.go
@@ -91,10 +91,7 @@ func Example_basicConfiguration() {
 	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
 		panic(err)
 	}
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
+	logger := zap.Must(cfg.Build())
 	defer logger.Sync()
 
 	logger.Info("logger construction succeeded")
@@ -278,10 +275,7 @@ func ExampleAtomicLevel_config() {
 	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
 		panic(err)
 	}
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
+	logger := zap.Must(cfg.Build())
 	defer logger.Sync()
 
 	logger.Info("info logging enabled")
