f2f1f41bca85b8a97b1bc90f24eadf390c425a07
Jian Zeng <anonymousknight96@gmail.com>
2023-03-17 21:15:50 +0800

    [Feature] Add  experiment `zapslog` package to integrate with slog (#1246)
    Ref https://github.com/uber-go/zap/discussions/1179
    
    This PR provides a implementation of `slog.Handler` in a separate
    experiment `exp/zapslog` module as suggested by @abhinav.

---
+++ exp/zapslog/slog_test.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2023 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package zapslog
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
+	"golang.org/x/exp/slog"
+)
+
+func TestAddSource(t *testing.T) {
+	r := require.New(t)
+	fac, logs := observer.New(zapcore.DebugLevel)
+	sl := slog.New(HandlerOptions{
+		AddSource: true,
+	}.New(fac))
+	sl.Info("msg")
+
+	r.Len(logs.AllUntimed(), 1, "Expected exactly one entry to be logged")
+	entry := logs.AllUntimed()[0]
+	r.Equal("msg", entry.Entry.Message, "Unexpected message")
+	r.Regexp(
+		`/slog_test.go:\d+$`,
+		entry.Caller.String(),
+		"Unexpected caller annotation.",
+	)
+}
